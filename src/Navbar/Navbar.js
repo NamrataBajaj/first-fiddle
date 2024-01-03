9e7f1d21ef7a00ad2dec76f013e641a41dec37ba
NamrataBajaj <namrata.kodegurus@gmail.com>
2024-01-03 17:15:55 +0530

    Navbar changes

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
       <section className="bg-black text-white">
         <nav className="navbar navbar-expand-md navbar-dark">
           <div className="container-fluid">
-            <a href="this" className="navbar-brand">
+            <a href="/" className="navbar-brand">
               <img src="https://firstfiddle.in/_next/image?url=%2Fimages%2Fheader%2Flogo-01.png&w=256&q=75" width="40px" height="40px" alt="33" className="until img-fluid">
               </img>
             </a>
@@ -14,15 +14,14 @@ function Navbar() {
 
             <div className="collapse navbar-collapse" id="pn">
               <ul className="navbar-nav ms-auto">
-                <li><a href="/Home" className="nav-link text-white">HOME</a></li>
-                <li><a href="/Contact" className="nav-link smooth_scroll text-white">ABOUT</a></li>
-                <li><a href="/Brand" className="nav-link text-white">BRAND</a></li>
-                <li><a href="/Press" className="nav-link text-white">PRESS RELEASE</a></li>
-                <li><a href="/Details" className="nav-link text-white">CONTACT US</a></li>
+                <li><a href="/" className="nav-link text-white">HOME</a></li>
+                <li><a href="/about" className="nav-link smooth_scroll text-white">ABOUT</a></li>
+                <li><a href="/brand" className="nav-link text-white">BRAND</a></li>
+                <li><a href="/press" className="nav-link text-white">PRESS RELEASE</a></li>
+                <li><a href="/contactus" className="nav-link text-white">CONTACT US</a></li>
                 <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#aa">
                   Login
                 </button>
-
               </ul>
             </div>
 
